32113e06d4aa2d8a61ec2cf7698478f4190415f2
dangkhoa105 <dangkhoa.dk9@gmail.com>
2020-01-17 20:52:52 +0700

    first game

---
+++ Castle Vania/Animations.cpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "Game.h"
+#include "debug.h"
+#include "Animations.h"
+
+CAnimations* CAnimations::__instance = NULL;
+
+CAnimations* CAnimations::GetInstance()
+{
+	if (__instance == NULL) __instance = new CAnimations();
+	return __instance;
+}
+
+void CAnimations::Add(int id, LPANIMATION ani)
+{
+	animations[id] = ani;
+}
+
+LPANIMATION CAnimations::Get(int id)
+{
+	return animations[id];
+}
---
+++ Castle Vania/Sprites.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <Windows.h>
+#include <d3dx9.h>
+#include <unordered_map>
+#include "Sprite.h"
+
+using namespace std;
+
+/*
+	Manage sprite database
+*/
+class CSprites
+{
+	static CSprites * __instance;
+
+	unordered_map<int, LPSPRITE> sprites;
+
+public:
+	void Add(int id, int left, int top, int right, int bottom, LPDIRECT3DTEXTURE9 tex);
+	LPSPRITE Get(int id);
+	LPSPRITE &operator[](int id) {return sprites[id];}
+
+	static CSprites * GetInstance();
+};
+
+
+
